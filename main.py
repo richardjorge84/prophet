1178966f4e215369a4e19c93e035b593bf04f322
Jorge Richard Angulo <richardjorge84@gmail.com>
2018-08-06 15:48:14 -0500

    Update main.py
    Last changes submitted to CS50

--- main.py
+++ main.py
@@ -1,34 +1,45 @@
-# Implementation of FB Prophet for Time series prediction. Jorge Richard/2018.
-# Pandas is imported
+# Final CS50x Project. Jorge Richard Angulo/2018.
+# Pandas is imported to get data from 'history'.xlsx Sheet1 file
 import pandas as pd
-# Calling Prophet to create the model 
+# Prophet to be the FB API to call
 from fbprophet import Prophet
-# Call plots (based on matplotlib) to plot outcomes
-from fbprophet.plot import add_changepoints_to_plot
+# matplotlib to generate output chart to see the fit, forecasts and bounds
+import matplotlib.pyplot as plt
+# Numpy to select in data matrix the original values to avoid plot "ceros"
+import numpy as np
 
-# Call the data from *.csv file. 
-# The input to Prophet is always a dataframe with two columns: ds and y. 
-# The ds (datestamp) column should be of a format expected by Pandas, ideally YYYY-MM-DD for a date or YYYY-MM-DD HH:MM:SS for a timestamp. 
-# The y column must be numeric, and represents the measurement we wish to forecast.
-df = pd.read_csv('library.csv')
-# Validate the data
-print(df.head())
-# create the variable to hold the data
-m = Prophet()
-# fit over m the data pulled from csv
-m.fit(df)
-# Predict for the selected range (periods stated here as days. See documentation of prophet for other type of periods)
-future = m.make_future_dataframe(periods=180)
-print(future.tail())
-# Save on variable forecast the predictions
-forecast = m.predict(future)
-# Select the columns to visualize prelim, ds = date, yhat = forecast, yhat_lower = lower limit, yhat_upper = upper limit
-forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
-# Plot the forecast o fig1. Then add the main trend changes.
-fig1 = m.plot(forecast)
-a = add_changepoints_to_plot(fig1.gca(), m, forecast)
-# Plot the components of the prediction, trend, seasonality by YYYY, MM, Week and DD.
-fig2 = m.plot_components(forecast)
-# Write the output predictions as csv
-forecast.to_csv("forecastout.csv")
 
+def main():
+
+    # Get the data from excel file called 'history.xlsx' on Sheet 1; cell A1=ds and A2=y
+    data = pd.read_excel('history.xlsx', sheet_name='Sheet1')
+    # Calling the Prophet model. See other seasonalities in FB Prophet documentation
+    model = Prophet(yearly_seasonality=25, weekly_seasonality=25)
+    model.fit(data)
+    # Cutomize number of days if needed
+    num_days = 180
+    future = model.make_future_dataframe(periods=num_days)
+    forecast = model.predict(future)
+    data.set_index('ds', inplace=True)
+    forecast.set_index('ds', inplace=True)
+    full_matrix = data.join(forecast[['yhat', 'yhat_lower', 'yhat_upper']], how='outer')
+    # Change "M" (for months) to "W" for weeks or "D" for days
+    full_matrix = full_matrix.resample("M").sum()
+    original_value_count = np.count_nonzero(full_matrix['y'])
+    # Chart names can be customized in this section
+    full_matrix['Original (y)'] = full_matrix['y'][0:original_value_count]
+    full_matrix['Forecasts'] = full_matrix['yhat']
+    full_matrix['LCL'] = full_matrix['yhat_lower']
+    full_matrix['UCL'] = full_matrix['yhat_upper']
+    full_matrix[['Original (y)', 'Forecasts', 'LCL', 'UCL']].plot()
+    plt.legend(loc=0)
+    # This will save a png file to see the fit, forecast and bounds
+    plt.savefig('prophet.png', dpi=200, bbox_inches='tight')
+    # The full excel file with forecast and bounds will be provided, gropued as per line 25 value
+    writer = pd.ExcelWriter('forecasts.xlsx')
+    full_matrix[['Original (y)', 'Forecasts', 'LCL', 'UCL']].to_excel(writer, 'Sheet1')
+    writer.save()
+
+
+if __name__ == "__main__":
+    main()
